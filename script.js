57ae53f599cb04c406cb14e0c70cf2a2fa0ac651
[Amar Malde] <105677445+AmarMalde@users.noreply.github.com>
2023-01-10 18:18:58 +0000

    added code to randomly shuffle the final password to ensure it is truly random

--- script.js
+++ script.js
@@ -99,26 +99,26 @@ function getPasswordOptions() {
   // setting charsArray to empty. This is important if we want to generate more than one password effectively
   charsArray = [];
   // setting charsArray to empty. This is important if we want to generate more than one password effectively
-  passwordValue = ""
+  passwordValue = "";
 
   // below ifs will prompt the user to choose each char type
   // if the user choosed a char type it will add that to charArray
   // it will also add 1 char of that type to the password (via the variable passwordValue)
   if (confirm("Would you like to include lower case characters?")) {
-    charsArray = charsArray.concat(lowerCasedCharacters)
-    passwordValue = passwordValue + getRandom(lowerCasedCharacters)
+    charsArray = charsArray.concat(lowerCasedCharacters);
+    passwordValue = passwordValue + getRandom(lowerCasedCharacters);
   };
   if (confirm("Would you like to include upper case characters?")) {
-    charsArray = charsArray.concat(upperCasedCharacters)
-    passwordValue = passwordValue + getRandom(upperCasedCharacters)
+    charsArray = charsArray.concat(upperCasedCharacters);
+    passwordValue = passwordValue + getRandom(upperCasedCharacters);
   };
   if (confirm("Would you like to include numneric characters?")) {
-    charsArray = charsArray.concat(numericCharacters)
-    passwordValue = passwordValue + getRandom(numericCharacters)
+    charsArray = charsArray.concat(numericCharacters);
+    passwordValue = passwordValue + getRandom(numericCharacters);
   };
   if (confirm("Would you like to include special characters?")) {
-    charsArray = charsArray.concat(specialCharacters)
-    passwordValue = passwordValue + getRandom(specialCharacters)
+    charsArray = charsArray.concat(specialCharacters);
+    passwordValue = passwordValue + getRandom(specialCharacters);
   };
 
   // This function will return the amount of digits the user would like. 
@@ -128,25 +128,37 @@ function getPasswordOptions() {
 }
 
 // Function for getting a random element from an array
+// it takes an array as a parameter
 function getRandom(arr) {
+  //this will return one random element from the array 
   return(arr[Math.floor(Math.random() * arr.length)]);
 }
 
 // Function to generate password with user input
 function generatePassword() {
-  var passwordValue = ""
+
+  // getPasswordOptions is ran, and it's return value is stored in digitsAmount to be used later in the for loop
   var digitsAmount = getPasswordOptions()
+
+  // validation check to ensure user has selected a char type
   if (charsArray.length == 0) {
-    alert("Whoops! You need to choose at least one character type")
-    return("Whoops! You need to choose at least one character type")
+    alert("Whoops! You need to choose at least one character type");
+    return("Whoops! You need to choose at least one character type");
   };
+  // validation check to ensure user has chosen a valid length for array and returns the error if they have't
   if (digitsAmount < 10 || digitsAmount > 64) {
-    alert("Whoops! Your password length must have at least 10 chars and no more than 64")
-    return("Whoops! Your password length must have at least 10 chars and no more than 64")
+    alert("Whoops! Your password length must have at least 10 chars and no more than 64");
+    return("Whoops! Your password length must have at least 10 chars and no more than 64");
   };
 
+  // using digitsAmount as a limit the for loop will add a random char to the array using getRandom()
   for (var i = 0; i < digitsAmount; i++) {passwordValue = passwordValue + getRandom(charsArray)}
 
+  // below code turns the string into an array in order to use sort and math.random to randomise the order of the chars in the string
+  // it then turns it from an array back into a string
+  var randomPass = passwordValue.split('').sort(function(){return 0.5-Math.random()}).join('');
+
+  // returns the password
   return(passwordValue)
 
 }
